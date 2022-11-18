b20e5b57393e20b26d71bab978b762f35f40e440
Ismatillo Marufkhonov <marufkhonovismatillo@gmail.com>
2022-11-18 21:01:30 +0500

    Initial Commit

---
+++ Zero/ZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroApp.swift
+//  Zero
+//
+//  Created by Ismatilla on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
